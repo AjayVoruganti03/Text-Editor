0352d31fb523c5ce166f09b6f832e41fe69ca15c
AjayVoruganti03 <113462338+AjayVoruganti03@users.noreply.github.com>
2023-04-25 02:07:27 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
-import React, { useState } from "react";
-import TextEditor from "./TextEditor";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import TextEditor from "./TextEditor"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom"
+// import { v4 as uuidV4 } from "uuid"
 
 function App() {
-  const [roomId, setRoomId] = useState(""); // Initialize roomId state
 
-  // Handler function to update roomId state when needed
-  const handleSetRoomId = (id) => {
-    setRoomId(id);
-  };
 
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          {/* Redirect to the TextEditor component with the current roomId state */}
-          <Redirect to={`/documents/${roomId}`} />
-        </Route>
-        <Route path="/documents/:id">
-          {/* Pass the roomId state as a prop to the TextEditor component */}
-          <TextEditor roomId={roomId} setRoomId={handleSetRoomId} />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    <Switch>
+      <Route path="/" exact>
+        {/* Always redirect to the same document */}
+        <Redirect to={`/documents/12345`} />
+      </Route>
+      <Route path="/documents/:id">
+        <TextEditor />
+      </Route>
+    </Switch>
+  </Router>
+  )
 }
 
-export default App;
+export default App
